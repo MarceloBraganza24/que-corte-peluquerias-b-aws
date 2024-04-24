86a17b84c2bf75c6bb9da17e171b955fdf8a9ac8
litocode24 <marcelo_braganza@hotmail.com>
2024-04-23 22:04:30 -0300

    update tickets.router.js

--- src/routes/tickets.router.js
+++ src/routes/tickets.router.js
@@ -2,7 +2,7 @@ import Router from "./router.js";
 import { accessRolesEnum, passportStrategiesEnum } from "../config/enums.js";
 import { getAll, getById, save } from '../controllers/tickets.controller.js';
 
-export default class SessionsRouter extends Router {
+export default class TicketsRouter extends Router {
     init() {
         this.get('/', [accessRolesEnum.ADMIN, accessRolesEnum.PREMIUM], passportStrategiesEnum.JWT, getAll);
         this.get('/:tid', [accessRolesEnum.ADMIN, accessRolesEnum.PREMIUM], passportStrategiesEnum.JWT, getById);
